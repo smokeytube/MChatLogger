9defca412303903aecdfb200cb1aac63c4da0e4c
smokeytube <67290867+smokeytube@users.noreply.github.com>
2021-01-08 16:47:20 -0500

    Intergrated sql into main

--- logger.py
+++ logger.py
@@ -1,8 +1,8 @@
 import os
 import time
-from datetime import datetime
-import time
 import sqlite3
+from datetime import datetime, date
+import time
 
 def follow(thefile):
     thefile.seek(0,2)
@@ -16,7 +16,35 @@ def follow(thefile):
 
 logfile = open(os.getenv("APPDATA")+"/.minecraft/logs/latest.log", "r")
 loglines = follow(logfile)
+x = 1
 for line in loglines:
     print (line)
     now = datetime.now()
-    current_timeint = now.strftime("%H%M")
+    current_time = now.strftime("%H_%M")
+
+    today = date.today()
+
+    conn = sqlite3.connect(f'log{today}.db')
+
+    c= conn.cursor()
+
+    if x == 1:
+        try:
+            c.execute(f"""CREATE TABLE log{current_time} (
+                        playername text,
+                        message text,
+                        other text,
+                        timestamp text
+                )""")
+            x = x+1
+        except:
+            pass
+
+    c.execute(f"INSERT INTO log{current_time} VALUES ({line.split(']')[2]}, 'i am popbob', '', 'hi')")
+    c.execute(f"SELECT * FROM log{current_time}")
+
+    print(c.fetchall())
+
+    conn.commit()
+
+    conn.close()
