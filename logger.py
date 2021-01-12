5ea7b3f0afbd24156fa5afbaf356756872bffd10
smokeytube <67290867+smokeytube@users.noreply.github.com>
2021-01-11 23:05:50 -0500

    Fully functional now. happy logging!
    Ignore junk data

--- logger.py
+++ logger.py
@@ -20,12 +20,14 @@ loglines = follow(logfile)
 cwd = os.getcwd()
 
 try:
-    filelog = open(f'{cwd}/{datetime.today().year}/{datetime.today().month}/{datetime.today().day}/log.txt', 'a+')
+    filelog = open(f'{cwd}/{datetime.today().year}/{datetime.today().month}/{datetime.today().day}/{datetime.now().strftime("%H-00")}.txt', 'a+')
 except FileNotFoundError:
     os.makedirs(f'{cwd}/{datetime.today().year}/{datetime.today().month}/{datetime.today().day}/')
-    filelog = open(f'{cwd}/{datetime.today().year}/{datetime.today().month}/{datetime.today().day}/log.txt', 'a+')
+    filelog = open(f'{cwd}/{datetime.today().year}/{datetime.today().month}/{datetime.today().day}/{datetime.now().strftime("%H-00")}.txt', 'a+')
 except:
     ctypes.windll.user32.MessageBoxW(0, u"An unexpected error occured. Reporting problem to mircosoft", u"what", 0)
+    filelog = 69
+    time.sleep(69420)
 
 
 x = 1
@@ -47,10 +49,10 @@ for line in loglines:
     if x > 10:
         filelog.close()
         try:
-            filelog = open(f'{cwd}/{datetime.today().year}/{datetime.today().month}/{datetime.today().day}/log.txt', 'a+')
+            filelog = open(f'{cwd}/{datetime.today().year}/{datetime.today().month}/{datetime.today().day}/{datetime.now().strftime("%H-00")}.txt', 'a+')
         except FileNotFoundError:
             os.makedirs(f'{cwd}/{datetime.today().year}/{datetime.today().month}/{datetime.today().day}/')
-            filelog = open(f'{cwd}/{datetime.today().year}/{datetime.today().month}/{datetime.today().day}/log.txt', 'a+')
+            filelog = open(f'{cwd}/{datetime.today().year}/{datetime.today().month}/{datetime.today().day}/{datetime.now().strftime("%H-00")}.txt', 'a+')
         except:
             ctypes.windll.user32.MessageBoxW(0, u"An unexpected error occured. Reporting problem to mircosoft", u"what", 0)
         x = 1
@@ -58,7 +60,7 @@ for line in loglines:
         x += 1
 
     # now = datetime.now()
-    # current_time = now.strftime("%H:%M")
+    # current_time = datetime.now().strftime("%H-%M")
 
     # now = datetime.now()
     # current_timeHR = now.strftime("%H")
